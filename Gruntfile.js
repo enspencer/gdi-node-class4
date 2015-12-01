a9d5f11fe66a5b75e78a6566e3c0a52c73053c42
enspencer <emma.n.spencer@gmail.com>
2015-12-01 18:38:47 -0500

    grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+  // 1st parameter: task command
+  // 2nd parameter: description
+  // 3rd parameter: function to run when `grunt default` is entered
+  grunt.registerTask('default', 'this task says hello', function() {
+    console.log('Hello!');
+  });
+
+  grunt.registerTask('greeting', 'say hello', function(name) {
+    if (!name || !name.length) {
+      grunt.warn('Please enter a name in the format grunt greeting:name');
+    }
+
+    console.log('Hello ' + name);
+  });
+
+  // Chaining Tasks
+
+
+
+
+
+  // grunt.initConfig({
+  //   htmlhint: {
+  //     build: {
+  //       options: {
+  //         'tag-pair': true,
+  //         'tagname-lowercase': true,
+  //         'attr-lowercase': true,
+  //         'attr-value-double-quotes': true
+  //       },
+  //       src: ['index.html']
+  //     }
+  //   },
+  //   watch: {
+  //   }
+  // });
+
+};
